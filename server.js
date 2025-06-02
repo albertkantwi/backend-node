fb3ddd419ea320c27d88f60ae0ddc5ae93d9fc03
Albert <albertkantwi@gmail.com>
2025-06-02 01:34:01 +0000

    Update router to add API end point

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const app = express()
 const PORT = 8383
 
 app.get('/', (req, res) => {
-    console.log('I hit an end point', req.method)
-    res.sendStatus(200)
+    res.send('<h1>Home page</h1>')
+})
+
+app.get('/dashboard', (req, res) => {
+    res.send("<h1>Dashboard</h1>")
+})
+
+app.get('/api/data', (req, res) => {
+    res.send('data')
 })
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
